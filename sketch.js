2fd5c40de852194022fe230afa3fab7b0f625eab
nhanLe <35633508+nhan-hub@users.noreply.github.com>
2020-04-02 15:19:23 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,16 +1,15 @@
-var particleNum = 2000;
+var particleNum = 3000;
 var particles = [];
 var flowfield;
 var canvas;
 var res = 12; 
 
 function setup() {
-    canvas = createCanvas(windowWidth*.9 , windowHeight );
-    canvas.style("position","absolute");
+    canvas = createCanvas(windowWidth , windowHeight );
+    canvas.style("position:fixed")
     canvas.style("top", "0");
     canvas.style("left", "0");
     canvas.style("z-index", "-1");
-    canvas.parent('section1');
 
   colorMode(HSB, 255);
 
@@ -18,9 +17,11 @@ function setup() {
   for ( var i=0; i<particleNum; i++) {    
     particles[i] = new Particle();
   } 
-  flowfield = new Field(); // Vector Field
-  flowfield.update();
 
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
@@ -28,7 +29,6 @@ function draw() {
   rect(0,0,width,height);
   // colCounter += 1;
   // colorChange(); 
-  flowfield.update();  
   for (var i = 0; i < particles.length; i++) {  
     // particles[i].follow(flowfield);
     particles[i].run();
