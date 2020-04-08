dcdaf654ac4fe6a899460cb6e1ebf3477d05bb1b
nhanLe <35633508+nhan-hub@users.noreply.github.com>
2020-04-08 00:47:52 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -5,19 +5,16 @@ var canvas;
 var res = 12;
 
 function setup() {
-    canvas = createCanvas(windowWidth , windowHeight );
-    canvas.style("position:fixed")
-    canvas.style("top", "0");
-    canvas.style("left", "0");
-    canvas.style("z-index", "-1");
+  canvas = createCanvas(windowWidth , windowHeight ,P2D);
+  canvas.style("position:fixed")
+  canvas.style("top", "0");
+  canvas.style("left", "0");
+  canvas.style("z-index", "-1");
 
   colorMode(HSB, 255);
-
-
   for ( var i=0; i<particleNum; i++) {
     particles[i] = new Particle();
   }
-
 }
 
 function windowResized() {
@@ -27,11 +24,12 @@ function windowResized() {
 function draw() {
   fill(0,100);
   rect(0,0,width,height);
-  // colCounter += 1;
-  // colorChange();
   for (var i = 0; i < particles.length; i++) {
-    // particles[i].follow(flowfield);
     particles[i].run();
   }
+}
 
-  }
+function mousePressed(){
+  particles[i].explosion();
+  console.log("hello");
+}
